5f7e20a1e3c775be1086196eb7622d77c490baf2
Rahul Yedida <yrahul3910@gmail.com>
2020-08-18 00:59:02 -0400

    update

--- src/clients/drcctlib_memory_footprint/drcctlib_memory_footprint.cpp
+++ src/clients/drcctlib_memory_footprint/drcctlib_memory_footprint.cpp
@@ -53,8 +53,23 @@ InstrumentPerInsCache(void *drcontext, context_handle_t ctxt_hndl, int32_t mem_r
     per_thread_t* dat = (per_thread_t*) data;
     mem_ref_t* buf_ptr = *(mem_ref_t **)(byte*) dat->seg_base;
 
+    try {
+	std::cout << "Start: " << (mem_ref_t*) dat->buf_base->addr << std::endl;
+    } catch (...) {
+	std::cout << "Failed at pt 1\n" << std::endl;
+    }
+
+    try {
+	std::cout << "End: " << (mem_ref_t*) dat->buf_base->addr << std::endl;
+    } catch (...) {
+	std::cout << "Failed at pt 2\n" << std::endl;
+    }
+
+
+    int i = 0;
     for (mem_ref_t* mem_ref = (mem_ref_t*) dat->buf_base; mem_ref < buf_ptr; ++mem_ref)
     {
+	std::cout << "iter " << ++i << std::endl;
         std::cout << mem_ref->addr;
         std::cout << std::endl;
     }
